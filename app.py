c68fa6c0ba35832650f410ad370065007dc528fc
Pranshu <prop7533@gmail.com>
2023-12-08 21:55:51 +0530

    Commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 import os
@@ -13,44 +13,51 @@ app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
 
+
+def time():
+    utc = datetime.utcnow()
+    ist = timedelta(days=0, hours=5, minutes=30, seconds=0)
+    utctoist = utc + ist
+    time = utctoist.strftime("%Y-%m-%d %I:%M:%S %p")
+    return time
+
+
 # Storing todo tasks
 class ToDo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.String(30))
     
-    dt = timedelta(hours=5, minutes=30) + datetime.utcnow()
-    nt= dt.strftime("%Y-%m-%d %I:%M:%S %p %Z")
-    date_created = db.Column(db.String(20), default=nt)
-    def date_created_ist(self):
-
-        ist_offset = timedelta(hours=5, minutes=33)
-        return self.date_created + ist_offset  
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
     
 
-# Tracing users ip addresses
 class VisitorLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
     ip_address = db.Column(db.String(20))
-    visited = db.Column(db.String(60))
+    device = db.Column(db.String(60))
     page = db.Column(db.String(30))
-    dt = timedelta(hours=5, minutes=30) + datetime.utcnow()
-    nt= dt.strftime("%Y-%m-%d %I:%M:%S %p %Z")
-    date_created = db.Column(db.String(20), default=nt)
-    def date_created_ist(self):
+    ist_time = db.Column(db.String(30))
 
-        ist_offset = timedelta(hours=5, minutes=33)
-        return self.date_created + ist_offset
 
-    def __repr__(self):
-        return f"<VisitorLog {self.ip_address}>"
+
+def log_visitor(ip_address, device, page, time):   
+    new_log = VisitorLog(
+        ip_address=ip_address,
+        page=page,
+        device = device,
+        ist_time = time
+    )
+    db.session.add(new_log)
+    db.session.commit()
+
+
 
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
+    print(request.headers)
     if request.method == "POST":
         Title = request.form["title"]
         Desc = request.form["desc"]
@@ -59,19 +66,20 @@ def hello():
             alltodo = ToDo.query.all()
             return render_template('index.html',title=False,  alltodo=alltodo)
         else:
-            todo = ToDo(title=Title, desc=Desc)
+            todo = ToDo(title=Title, desc=Desc, date_created = time())
             db.session.add(todo)
             db.session.commit()
             
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    user_agent = request.headers.get('User-Agent')
     page = "Home"
-    oip = VisitorLog(ip_address=ip, page=page)
-    db.session.add(oip)
-    db.session.commit()
+    ist = time()
+    log_visitor(ip, user_agent ,page , ist)
     
+    ip_add = VisitorLog.query.all()
     alltodo = ToDo.query.all()
     # print(alltodo)
-    return render_template('index.html', alltodo=alltodo)
+    return render_template('index.html',ip_add=ip_add, alltodo=alltodo)
     # return "Hello World!" 
 
 # to edit a task
@@ -160,6 +168,6 @@ def dev():
 
 if __name__ == "__main__":
     db.create_all()
-    app.run()
+    app.run(debug=True)
 
 
